440dc9d252ba7382e15c520a6965d2d02f7409a2
Mayy isme <3hung1998@gmail.com>
2023-10-27 00:43:35 +0700

    source day3

--- video 2/video2.js
+++ video 2/video2.js
@@ -1,2 +1,6 @@
-console.log("hello world from JS");
-console.log("hello Mayy");
+// console.log("hello world from JS");
+// console.log("hello Mayy");
+let a = " Toi la String";
+console.log(a);
+console.log(4 + 5 + "a");
+console.log("a" + 5 + 4);

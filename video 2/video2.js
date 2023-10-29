e514c95acb6f2a6b6eaf320f14a97b4edd5f6ba0
Mayy isme <3hung1998@gmail.com>
2023-10-29 23:12:50 +0700

    source day4

--- video 2/video2.js
+++ video 2/video2.js
@@ -1,6 +1,9 @@
 // console.log("hello world from JS");
 // console.log("hello Mayy");
 let a = " Toi la String";
-console.log(a);
-console.log(4 + 5 + "a");
-console.log("a" + 5 + 4);
+// console.log(a);
+// console.log(4 + 5 + "a");
+// console.log("a" + 5 + 4);
+let text = "Tu hoc JS";
+console.log(text.length);
+console.log(text.replace("JS", "Code"));

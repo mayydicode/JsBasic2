70a085030fee8de445fb8cae86edc89eb9fc5360
Mayy isme <3hung1998@gmail.com>
2023-10-31 00:04:58 +0700

    source day4

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ let a = " Toi la String";
 // console.log(4 + 5 + "a");
 // console.log("a" + 5 + 4);
 let text = "Tu hoc JS";
-console.log(text.length);
-console.log(text.replace("JS", "Code"));
-console.log(text.toUpperCase());
-console.log(a);
+// console.log(text.length);
+// console.log(text.replace("JjS", "Code"));
+// console.log(text.toUpperCase());
+// console.log(a);
+// Opject
+let b = {
+  name: "Mayy",
+  address: "Hue",
+};
+console.log("Ten toi la:", b.name);
+
+// <--Array-->
+let pet = {
+  dog: "lu",
+  cat: "meo",
+};
+console.log("day la con cho ten:", pet);

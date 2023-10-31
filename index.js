aa4785e6bc5cf6a0c45cafde3d4cd03c5472d906
Mayy isme <3hung1998@gmail.com>
2023-11-01 01:21:18 +0700

    source day4

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ let a = " Toi la String";
 // console.log("a" + 5 + 4);
 let text = "Tu hoc JS";
 // console.log(text.length);
-// console.log(text.replace("JjS", "Code"));
 // console.log(text.toUpperCase());
 // console.log(a);
 // Opject
@@ -20,4 +19,20 @@ let pet = {
   dog: "lu",
   cat: "meo",
 };
-console.log("day la con cho ten:", pet);
+// console.log("day la con cho ten:", pet);
+
+// <--Empty,NULL,Underfined-->
+
+// <--For Looops-->
+// console.log("Hello world ");
+for (let i = 1; i <= 10; i++) {
+  // console.log("check variable i:", i);
+}
+
+let arr = ["MU", "Mcty", "Liver", "chelsea"];
+for (let i = 0; i < arr.length; i++) {
+  // console.log("Cac doi bong :", arr[i]);
+}
+
+// <--While/Do While Loops-->
+let arrtop4 = ["MU", "Mcty", "Liver", "chelsea"];

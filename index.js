bb5f2f47c4e7f2ef34e291a89bc2c6398b48dca4
Mayy isme <3hung1998@gmail.com>
2023-11-02 00:59:37 +0700

    source day4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let b = {
   name: "Mayy",
   address: "Hue",
 };
-console.log("Ten toi la:", b.name);
+// console.log("Ten toi la:", b.name);
 
 // <--Array-->
 let pet = {
@@ -24,15 +24,29 @@ let pet = {
 // <--Empty,NULL,Underfined-->
 
 // <--For Looops-->
-// console.log("Hello world ");
+console.log("Hello world ");
 for (let i = 1; i <= 10; i++) {
   // console.log("check variable i:", i);
 }
 
-let arr = ["MU", "Mcty", "Liver", "chelsea"];
-for (let i = 0; i < arr.length; i++) {
-  // console.log("Cac doi bong :", arr[i]);
-}
+// let arr4 = ["MU", "Mcty", "Liver", "chelsea"];
+// for (let i = 0; i < arr4.length; i++) {
+//   console.log("Cac doi bong :", arr4[i]);
+// }
 
 // <--While/Do While Loops-->
-let arrtop4 = ["MU", "Mcty", "Liver", "chelsea"];
+let arr = ["MU", "Mcty", "Liver", "chelsea"];
+
+// <--If/else-->
+let arrTop4 = ["live", "Chelsea", "MU", "Man city"];
+
+let i = 0;
+while (i < arrTop4.length) {
+  if (arrTop4[i].length === 2) {
+    console.log("Top :", i + 1, arrTop4[i]);
+  } else if (arrTop4[i].length === 7) {
+    console.log("top :", i + 1, arrTop4[i]);
+  } else {
+  }
+  i++;
+}
